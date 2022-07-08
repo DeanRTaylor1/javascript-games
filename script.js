5d5f87376e8b5448073a17c657c8a645e317ada0
Dean Taylor <deanrtaylor@hotmail.com>
2022-07-08 16:13:12 +0700

    formatted code removed comments

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ let playerSelection
 
     console.log(computerSelection);
 
-function trimmer(s) { s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();}
-
-     
-        
+function trimmer(s) { s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();}   
 
         
         
@@ -235,113 +232,4 @@ function playRound (playerSelection, ComputerSelection){
 
 
 
-        console.log(btnRock, btnScissors, btnPaper);
-
-
-        //button.addEventListener()
-
-
-
-
-
-            
- 
-         
-
-/*function game () { //running the game 
-
-           
-            
-    let computerScore = 0; // variable to track the computer's points
-    
-    let playerScore = 0; // variable to track the player's score
-
-    let n;
-
-        for (let i = 0; i < 5; i++) {              // for loop to play five rounds     
-                    
-            playerSelection = prompt("Choose your weapon", 'Rock')
-
-            // playerSelection = trimmer(playerInput);                     
-
-            n = computerPlay(options + i);  // I'm not sure if this is correct but it seems to give the correct functionality, one isse i was having is that the computer selection was the same for all rounds, adding the i variable gave me new options.
-
-            console.log(n); // debugging check the computer's selection
-                    
-            // console.log(playRound(playerSelection, n));
-                    
-            if ((playRound(playerSelection, n) === 0)) {computerScore += 1; playerScore += 1; console.log(`It's a tie ${playerSelection} and ${n} are the same!`);}
-
-            else if ((playRound(playerSelection, n) === -1)) {computerScore += 1; console.log(`You lose ${playerSelection} is beaten by ${n}`);}
-
-             else if ((playRound(playerSelection, n) === 1)) {playerScore += 1; console.log(`You win! ${playerSelection} beats ${n}!`);}
-                    
-                   //console.log(score); 
-                console.log(computerScore);
-                console.log(playerScore);
-                                        
-        }
-
-        if (computerScore === playerScore){
-
-            console.log(`It's a tie! ${computerScore} points each!`);
-
-        }
-
-        else if (computerScore > playerScore){
-
-                    console.log(`You lose! the computer got ${computerScore} points and you got ${playerScore} points`);
-
-         }
-
-        else console.log(`You win! the computer got ${computerScore} points and you got ${playerScore} points`);
-        }
-        
-
-      
-game();*/
-
-
-
-/*const container = document.querySelector('#container');
-
-const content = document.createElement('div');
-
-content.style.cssText = "background-color: pink;";
-
-content.classList.add('content');
-content.innerHTML = "<h1>I'm in a div</h1><p>Me too!</p>";
-
-container.appendChild(content);
-
-const newContainer = document.querySelector('#container');
-
-const newContent = document.createElement('div');
-
-newContent.style.cssText = "background-color: gray;";
-
-newContent.classList.add('content');
-newContent.innerHTML = "<h1>I'm in a div</h1><p>Me too!</p>";
-
-newContainer.appendChild(newContent);
-
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
-  });
-
-  // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.id);
-  });
-});*/
-
-
-
+       
